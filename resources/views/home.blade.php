648b584c2b814ec19c4955a98b3edb6788f53c5e
camillamanzo <camillasouthafrica@gmail.com>
2021-10-25 21:48:00 +0200

    initialized exercise

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main');
+
+@section('content')
+    <h1>test</h1>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
+    <title>Comics</title>
+</head>
+<body>
+    @include('partials.header')
+    
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+    @yield('script-adder')
+</body>
+</html>
